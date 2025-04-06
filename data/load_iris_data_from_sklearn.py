88b5e386fa6b8f3cf3d30fe0d90dbe5ed1d01a6d
bharat15-ml <bharatbhushansingh2019@gmail.com>
2025-04-06 04:55:19 -0700

    moved load_iris_data_from_sklearn.py file to scripts folder

--- data/load_iris_data_from_sklearn.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from sklearn.datasets import load_iris
-
-# Load Iris dataset
-iris = load_iris()
-df = pd.DataFrame(data=iris['data'],columns = iris['feature_names'])
-df['species'] = pd.Categorical.from_codes(iris.target, iris.target_names)
-df.to_csv('data/iris.csv', sep = ',', index = False)
